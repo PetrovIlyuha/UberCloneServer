5608b689a1e5841ada3770ed82d9a7613274e360
Ilya Petrov <dnamix1@gmail.com>
2019-12-06 10:54:08 +0300

    added user verification model for GraphQL API

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,7 +1,38 @@
-export const typeDefs = ["type Query {\n  sayBye: String!\n  sayHello: String!\n}\n"];
+export const typeDefs = ["type User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstname: String!\n  lastName: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  createdAt: String\n  updatedAt: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLong: Float!\n  lastLat: Float!\n  lastOrientation: Float\n}\n\ntype Query {\n  user: User\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  createdAt: String!\n  updatedAt: String!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
-  sayBye: string;
-  sayHello: string;
+  user: User | null;
+}
+
+export interface User {
+  id: number;
+  email: string | null;
+  verifiedEmail: boolean;
+  firstname: string;
+  lastName: string;
+  age: number | null;
+  password: string | null;
+  phoneNumber: string | null;
+  verifiedPhoneNumber: boolean;
+  profilePhoto: string | null;
+  createdAt: string | null;
+  updatedAt: string | null;
+  fullName: string | null;
+  isDriving: boolean;
+  isRiding: boolean;
+  isTaken: boolean;
+  lastLong: number;
+  lastLat: number;
+  lastOrientation: number | null;
+}
+
+export interface Verification {
+  id: number;
+  target: string;
+  payload: string;
+  key: string;
+  used: boolean;
+  createdAt: string;
+  updatedAt: string;
 }
