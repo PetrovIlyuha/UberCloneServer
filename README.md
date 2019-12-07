3e6575c1d3ec240b194e8eb0ceffe7ac82986628
Ilya Petrov <dnamix1@gmail.com>
2019-12-07 12:06:27 +0300

    added facebook sign-in schema and resolver with GraphQL and typeorm

---
+++ README.md
@@ -0,0 +1,36 @@
+## Resolvers
+
+### Public Resolvers
+
+- [x] Sign In / Sign Up with FaceBook
+- [] Sign In with Email
+- [] Starts Phone Number Verification
+- [] Complete Phone Number Verification
+- [] Sign Up with Email
+
+---
+
+### Private Resolvers
+
+- [] Verify Email
+- [] Get My Profile
+- [] Update My Profile
+- [] Toggle Driving Mode
+- [] Report Location / Orientation
+- [] Add Place
+- [] Edit Place
+- [] Delete Place
+- [] See nearby drivers
+- [] Subscribe to nearby drivers
+- [] Request a Ride
+- [] Get nearby Rides Requests
+- [] Subscribe to nearby Rides Requests
+- [] Subscribe to Ride Status
+- [] Get Chat Room Messages
+- [] Subscribe to Chat Room Messages
+- [] Send a Chat Message
+
+## Code Challenge
+
+- [] Get Ride History
+- [] See Ride Detail
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -15,8 +15,8 @@ import {
 
 import Chat from "./Chat";
 import Message from "./Message";
-import Verification from "./Verification";
 import Ride from "./Ride";
+import Verification from "./Verification";
 
 const BCRYPT_ROUNDS = 10;
 
@@ -24,9 +24,9 @@ const BCRYPT_ROUNDS = 10;
 class User extends BaseEntity {
   @PrimaryGeneratedColumn() id: number;
 
-  @Column({ type: "text", unique: true })
+  @Column({ type: "text", nullable: true })
   @IsEmail()
-  email: string;
+  email: string | null;
 
   @Column({ type: "boolean", default: false })
   verifiedEmail: boolean;
@@ -37,13 +37,13 @@ class User extends BaseEntity {
   @Column({ type: "text" })
   lastName: string;
 
-  @Column({ type: "int" })
+  @Column({ type: "int", nullable: true })
   age: number;
 
-  @Column({ type: "text" })
+  @Column({ type: "text", nullable: true })
   password: string;
 
-  @Column({ type: "text" })
+  @Column({ type: "text", nullable: true })
   phoneNumber: string;
 
   @Column({ type: "boolean", default: false })
@@ -73,6 +73,9 @@ class User extends BaseEntity {
 
   @Column({ type: "double precision", default: 0 })
   lastOrientation: number;
+
+  @Column({ type: "text", nullable: true })
+  fbId: string;
 
   @ManyToOne(
     type => Chat,
